c4adaa8e7a24e9b987161ed87234b3667d8d61b4
Abhishek Bhavsar <abhi.n.bhavsar@gmail.com>
2023-08-07 22:00:01 +0530

    added db models

--- Django-Workspace/blogger/blog/admin.py
+++ Django-Workspace/blogger/blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+import blog.models as blog_models
 # Register your models here.
+
+admin.site.register(blog_models.Post)
+admin.site.register(blog_models.Author)
+admin.site.register(blog_models.Tag)
